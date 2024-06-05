cce413375c0b1e612d319976161f27c5464596bb
Kushagra-2023 <extraordinarykushagra@gmail.com>
2024-06-05 22:54:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import LoginForm from './components/LoginForm/LoginForm';
+import SignupForm from './components/SignupForm/SignupForm';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div>
+    //   <LoginForm />
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+        {/* <Route path="/main-page-1" component={MainPage1} />
+        <Route path="/main-page-2" component={MainPage2} />
+        <Route path="/" exact component={LoginPage} /> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import './SignupForm.css';
+import { FaUser, FaLock } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+
+const LoginForm = () => {
+  return (
+    <div className="wrapper">
+      <form action="">
+        <h1>Signup</h1>
+        <div className="input-box">
+          <input type="text" placeholder="Username" required />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" required />
+          <FaLock className="icon" />
+        </div>
+
+        {/* <div className="remember-forgot">
+          <label>
+            <input type="checkbox" /> Remember me
+          </label>
+          <a href="#">Forgot password?</a>
+        </div> */}
+
+        <button type="submit">Register</button>
+
+        {/* <div className="register-link">
+          <p>Don't have an account? <a href="#">Register</a></p>
+        </div> */}
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
